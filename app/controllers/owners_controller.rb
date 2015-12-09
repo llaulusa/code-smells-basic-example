89b48b37e71082895700107822def54e7c7c89a7
Amy Eisenmenger <amy.eisenmenger@gmail.com>
2015-12-09 14:27:05 -0700

    owner_controller index

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,6 +1,6 @@
 class OwnersController < ApplicationController
   def index
-    @o = Owners.all.sort_by{|onr| onr.first_name }
+    @owners = Owner.all.sort_by{|owner| owner.first_name }
   end
 
   def show
